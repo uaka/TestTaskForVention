e8c7dd17dd4b3d6e08ae2f3ab04cc5eac1f3851e
darya <uakaama@gmail.com>
2025-05-13 16:01:09 +0000

    hooks updated

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -10,9 +10,7 @@ import {
 import { Browser, BrowserContext, Page, chromium } from 'playwright';
 import { CustomWorld } from '../world/customWorld';
 import * as path from 'path';
-import fs from 'fs';
 import { browserOptions } from '../config/browserConfig';
-// import { attachScreenshot, attachTrace } from './allureHelper';
 
 let browser: Browser;
 let context: BrowserContext;
@@ -22,23 +20,12 @@ BeforeAll(async function () {
     browser = await chromium.launch(browserOptions);
 });
 
-// BeforeAll(async () => {
-//     browser = await chromium.launch({
-//         logger: {
-//             isEnabled: (name, severity) => true,
-//             log: (name, severity, message, args) => console.log(`${name} ${message} ${args}`),
-//         },
-//     });
-// });
-
-
 Before(async function (this: any, scenario: ITestCaseHookParameter) {
     context = await browser.newContext();
     page = await context.newPage();
     await context.tracing.start({ screenshots: true, snapshots: true });
     this.page = page;
 
-    // Отключаем анимации
     await this.page.addInitScript(() => {
         document.body.style.setProperty('animation', 'none', 'important');
         document.body.style.setProperty('transition', 'none', 'important');
@@ -52,10 +39,10 @@ AfterStep(async function (this: CustomWorld, step) {
     }
 });
 
-// After(async () => {
-//     await context.close();
-// });
-//
-// AfterAll(async () => {
-//     await browser.close();
-// });
+After(async () => {
+    await context.close();
+});
+
+AfterAll(async () => {
+    await browser.close();
+});
